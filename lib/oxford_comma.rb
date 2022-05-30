c9fc63c18199afac0fb71c1f3fe46e1edf783efb
Luke Baltay <luke.baltay@yale.edu>
2022-05-29 21:33:49 -0400

    commit tree 38930568767dd3a94a35a89b0204b556ef220528

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,24 @@
 def oxford_comma(array)
 
+    if array.size==1
+        return array[0]
+    end
+
+    count = 1;
+    until count>=array.size - 1
+        w = array[count]
+        array[count] = " #{w}"
+        count+=1
+    end
+
+    
+    if array.size >2
+        w = array.pop
+        array << " and #{w}"
+        return array.join(",")
+    else
+        w = array.pop
+        array << " and #{w}"
+        return array.join
+    end
 end
